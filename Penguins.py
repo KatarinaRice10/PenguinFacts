81d831ef29d2461c0808de3760b637d0eaae3c16
KatarinaRice10 <113362963+KatarinaRice10@users.noreply.github.com>
2022-10-05 09:29:00 -0500

    Add files via upload

---
+++ Penguins.py
@@ -0,0 +1,3 @@
+myList = ["a", "King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Southern Rockhopper", "Macaroni"]
+choice = input("What penguin do you want to learn about?\n1. King\n2. Emperor\n3. Chinstrap\n4. Gentoo\n5. Little\n6. African\n7. Southern Rockhopper\n8. Macaroni\n")
+print(myList[choice])
