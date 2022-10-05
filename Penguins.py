a8fd1c287345cac1c36a109a1ef2354414312d30
KatarinaRice10 <820377@mystma.org>
2022-10-05 09:42:23 -0500

    .

--- Penguins.py
+++ Penguins.py
@@ -1,3 +1,12 @@
 myList = ["a", "King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Southern Rockhopper", "Macaroni"]
 choice = input("What penguin do you want to learn about?\n1. King\n2. Emperor\n3. Chinstrap\n4. Gentoo\n5. Little\n6. African\n7. Southern Rockhopper\n8. Macaroni\n")
 print(myList[choice])
+
+
+pokemon = ["char", "Hawl", "Garb", "Eeve"]
+pAtk = [84, 92, 95, 55]
+pType = ["fire", "flying", "poison", "normal"]
+p=0
+print(pokemon[int(p)])
+print(pAtk[int(p)])
+print(pType[int(p)])
